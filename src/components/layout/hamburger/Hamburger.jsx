c280ebc1b19edfb46b7d02e61153b0bb97333c7c
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-04-28 23:38:01 +0300

    routing

--- src/components/layout/hamburger/Hamburger.jsx
+++ src/components/layout/hamburger/Hamburger.jsx
@@ -1,6 +1,8 @@
 import { CgMenuRight } from 'react-icons/cg'
 import { IoClose } from 'react-icons/io5'
+
 import { useOnClickOutside } from '../../../hooks/useOnClickOutside'
+
 import styles from './Hamburger.module.scss'
 import Menu from './Menu'
 
@@ -10,7 +12,7 @@ const Hamburger = () => {
 	return (
 		<div className={styles.wrapper} ref={ref}>
 			<button onClick={() => setIsShow(!isShow)}>
-				{isShow ? <IoClose color='white' /> : <CgMenuRight color='white' />}
+				{isShow ? <IoClose /> : <CgMenuRight />}
 			</button>
 			<Menu isShow={isShow} />
 		</div>
